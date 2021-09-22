d36c1e568d0790534e962c2b382abb8da197696e
pdrakeesh <43811414+pdrakeesh@users.noreply.github.com>
2021-09-22 04:21:20 +0000

    Program without Recursive

--- main.cs
+++ main.cs
@@ -2,6 +2,20 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+   Console.WriteLine(factorial(5));
+  }
+
+  public static int factorial(int number)
+  {
+    int factorialNumber = number;
+    int incrementNumber = 1;
+    while(number > incrementNumber)
+    {
+      factorialNumber = factorialNumber * incrementNumber;
+      incrementNumber ++;
+      
+    }
+
+      return factorialNumber;
   }
 }
